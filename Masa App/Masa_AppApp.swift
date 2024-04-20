f19d6c19bfcff58d77e252af33943a0438941b5f
ModestSerhat <mdstsrht@outlook.com>
2024-04-20 15:52:42 +0300

    Initial Commit

---
+++ Masa App/Masa_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Masa_AppApp.swift
+//  Masa App
+//
+//  Created by Serhat İşbakan on 20.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct Masa_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
